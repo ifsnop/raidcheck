b757ae69f598447bb8bbdd4c34ec2212c3d16b5a
Diego Torres <diego.torres@gmail.com>
2014-02-02 19:26:59 +0000

    store modifications in sqlite database

--- monitord.py
+++ monitord.py
@@ -26,16 +26,10 @@ import sqlite3
 import pyinotify
 
 config = { 'db_file' : None,
-    'min_free_bytes' : 1024*1024*1024*5,
-    'min_size_bytes' : 0,
-    'max_size_bytes' : 1024*1024*1024,
     'recursive' : False,
-    'temp_path' : '/tmp',
     'watch_path' : './',
     'self': 'monitord.py'
     }
-
-valid_extensions = [ '.gps', '.sgps', '.bz2', '.ast', '.sast' ]
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_DELETE(self, event):
@@ -91,60 +85,6 @@ class EventHandler(pyinotify.ProcessEvent):
 
         if not stat.S_ISREG(filestat.st_mode):
             return False
-
-        # return True
-
-        print '{0} > filename({1}) sha1({2})'\
-                .format(format_time(), file['nameext'], sha1_file(file['pathnameext']))
-
-        if file['size']<config['min_size_bytes']:
-            print '{0} ? file size lower than ({1}): filename({2}) filesize({3}) extension({4})'\
-                .format(format_time(), config['min_size_bytes'], file['nameext'], \
-                format_size(file['size']), file['ext'])
-            return False
-
-        if file['size']>config['max_size_bytes']:
-            print '{0} ? file size greater than ({1}): filename({2}) filesize({3}) extension({4})'\
-                .format(format_time(), config['max_size_bytes'], file['nameext'], \
-                format_size(file['size']), file['ext'])
-            return False
-
-        if file['ext'] not in valid_extensions:
-            print '{0} ? not recognized extension: filename({1}) filesize({2}) extension({3})'\
-                .format(format_time(), file['nameext'], format_size(file['size']), file['ext'])
-            return False
-
-        ret = check_free_space([config['watch_path'], config['temp_path']], config['min_free_bytes'])
-        if isinstance(ret, basestring):
-            print '{0} ! ({1}) has less than {2} free'.format(format_time(), ret, format_size(config['min_free_bytes']))
-            return False
-
-        print '{0} > filename({1}) filesize({2}) extension({3})'\
-                .format(format_time(), file['nameext'], format_size(file['size']), file['ext'])
-
-        if file['ext'] == '.bz2':
-            print '{0} + ({1}) is a bz2 compressed file'.format(format_time(), file['nameext'])
-        elif file['ext'] == '.gps':
-            print '{0} + ({1}) is an operational recording file'.format(format_time(), file['nameext'])
-        elif file['ext'] == '.sgps':
-            print '{0} + ({1}) is a mode s recording file'.format(format_time(), file['nameext'])
-        else:
-            print '{0} + ({1}) is allowed but not action defined'.format(format_time(), file['nameext'])
-
-        match = re.match("(\d+)-(\S+)-(\d+)", file['name'])
-        if match is None:
-            print "{0} ! ({1}) can't be parsed as a valid (yymmdd-conf-hhmmss) filename"\
-                .format(format_time(), file['nameext'])
-
-            # let try the other way
-            match = re.match("(\d+)-(\d+)-(\S+)", file['name'])
-            if match is None:
-                print "{0} ! ({1}) can't be parsed as a valid (yymmdd-hhmmss-conf) filename"\
-                    .format(format_time(), file['nameext'])
-                return False
-
-        filename_extracted = match.groups()
-        pprint.pprint(filename_extracted)
 
         return True
 
@@ -224,34 +164,21 @@ def update_database(file):
 def main(argv):
     def usage():
         print 'usage: ', argv[0], '[-h|--help]'
-        print '                 [-f|--min-free]'
-        print '                 [-s|--min-size]'
-        print '                 [-m|--max-size]'
         print '                 [-r|--recursive]'
-        print '                 [-t|--temp-path <path>]'
         print '                 -w|--watch-path <path>'
         print
-        print 'Starts automatic SASS-C processes when new files are written'
+        print 'Starts automatic filesystm monitoring'
         print
         print ' -d, --db-file            sqlite path to store sha1 signatures'
         print '                          default to \'' + str(config['db_file']) + '\''
-        print ' -f, --min-free           minimum free space in watch & temp directories in bytes'
-        print '                          defaults to', format_size(config['min_free_bytes'])
-        print ' -s, --min-size           minimum file size to react, in bytes'
-        print '                          defaults to', format_size(config['min_size_bytes'])
-        print ' -m, --max-size           maximum file size to react, in bytes'
-        print '                          defaults to', format_size(config['max_size_bytes'])
         print ' -r, --recursive          descent into subdirectories'
         print '                          defaults to', str(config['recursive'])
-        print ' -t, --temp-path <path>   temporary path used to process new files'
-        print '                          defaults to \'' + config['temp_path'] + '\''
         print ' -w, --watch-path <path>  where to look for new files'
         print '                          defaults to \'' + config['watch_path'] + '\''
 
     try:
-        opts, args = getopt.getopt(argv[1:], 'hd:f:s:m:rt:w:', ['help',
-            'db-file=', 'min-free=', 'min-size=', 'max-size=', 'recursive',
-            'temp-path=', 'watch-path='])
+        opts, args = getopt.getopt(argv[1:], 'hd:rw:', ['help',
+            'db-file=', 'recursive', 'watch-path='])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -261,16 +188,6 @@ def main(argv):
             sys.exit()
         elif opt in ('-d', '--db-file'):
             config['db_file'] = arg
-        elif opt in ('-f', '--min-free'):
-            config['min_free_bytes'] = float(arg)
-        elif opt in ('-s', '--min-size'):
-            config['min_size_bytes'] = float(arg)
-        elif opt in ('-m', '--max-size'):
-            config['max_size_bytes'] = float(arg)
-        elif opt in ('-r', '--recursive'):
-            config['recursive'] = True
-        elif opt in ('-t', '--temp-path'):
-            config['temp_path'] = os.path.abspath(arg)
         elif opt in ('-w', '--watch-path'):
             config['watch_path'] = os.path.abspath(arg)
 
@@ -278,17 +195,8 @@ def main(argv):
 
     print '{0} > {1} init'.format(format_time(), config['self'])
     print '{0} > options: db-file({1})'.format(format_time(), config['db_file'])
-    print '{0} > options: min-free({1})'.format(format_time(), format_size(config['min_free_bytes']))
-    print '{0} > options: min-size({1})'.format(format_time(), format_size(config['min_size_bytes']))
-    print '{0} > options: max-size({1})'.format(format_time(), format_size(config['max_size_bytes']))
     print '{0} > options: recursive({1})'.format(format_time(), config['recursive'])
-    print '{0} > options: temp-path({1}) free_bytes({2})'.format(format_time(), config['temp_path'], format_size(get_free_space_bytes(config['temp_path'])))
     print '{0} > options: watch-path({1}) free_bytes({2})'.format(format_time(), config['watch_path'], format_size(get_free_space_bytes(config['watch_path'])))
-
-    ret = check_free_space([config['watch_path'], config['temp_path']], config['min_free_bytes'])
-    if isinstance(ret, basestring):
-        print '{0} ! ({1}) has less than {2} bytes'.format(format_time(), ret, format_size(config['min_free_bytes']))
-        sys.exit(3)
 
     if config['db_file'] is not None:
         conn = sqlite3.connect(config['db_file'], detect_types=sqlite3.PARSE_DECLTYPES)
